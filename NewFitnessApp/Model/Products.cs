15839601f612a7bad0df83184dcdb7e652ce7103
mekan <mekan@DESKTOP-SNTCQN1>
2022-04-06 01:15:46 +0300

    Добавьте файлы проекта.

---
+++ NewFitnessApp/Model/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewFitnessApp.Model
+{
+    public class Products
+    {
+        public string Name { get; }
+        public double weight { get; set; }//вес в граммах//сделать проверку 
+        public double CalloriesIn100Grams { get; }
+        public Products(string Name,double weight,double CalloriesIn100Grams) { }
+
+
+    }
+}
---
+++ NewFitnessApp/Model/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+/// <summary>
+/// описание класса пользователя
+/// </summary>
+namespace NewFitnessApp.Model
+{
+
+    public class User
+    {
+
+        private string Password { get; set; }
+        protected double DailyCalorieIntake;
+        public string Name { get; }
+        public DateTime BirthDay { get; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public double TypePhysicalActivity { get; }
+        public bool Gender { get; }//установка пола пользователя.1-мужчина.2-девушка
+        public User(string Name, DateTime BirthDay, double Weight, double Height, double TypePhysicalActivity, bool Gender,string Password)
+        { //надо добавит обработку данных на ошибки через throw
+            this.Name = Name;
+            this.BirthDay = BirthDay;
+            this.Weight = Weight;
+            this.Height = Height;
+            this.TypePhysicalActivity = TypePhysicalActivity;
+            this.Gender = Gender;
+            this.Password = Password;
+        }
+
+        public void CalculationDailyNormCalories(double Weight, double Height, double TypePhysicalActivity, DateTime BirthDay, double Gender)
+        {
+
+
+        }
+    }
+
+
+
+}
+
